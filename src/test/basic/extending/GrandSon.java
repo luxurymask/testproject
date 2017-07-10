767c115b5d822a78228ebbdf2401d2b7188a1b93
liuxl <luxurymask@163.com>
2017-07-10 10:24:19 +0800

    .

---
+++ src/test/basic/extending/GrandSon.java
@@ -0,0 +1,12 @@
+package test.basic.extending;
+
+public class GrandSon extends Son{
+
+	@Override
+	public void function() {
+		// TODO Auto-generated method stub
+		//i = 0;
+		j = 0;
+	}
+	
+}
---
+++ src/test/basic/extending/Son.java
@@ -0,0 +1,5 @@
+package test.basic.extending;
+
+public abstract class Son extends Father{
+	public abstract void function();
+}
